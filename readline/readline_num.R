554e2b562f99ad10728a4333cb24f8b61fe86f45
hihumi <sth@sth-no-MacBook-ProProPro.local>
2016-11-29 13:36:15 +0900

    make dir and file

---
+++ readline/readline_num.R
@@ -0,0 +1,14 @@
+  #' 入力された数値を出力
+  #'
+  #' @description: 省略
+  #'
+  #' @param: なし
+  #' @return: なし
+  #' @example: readline_num()
+  #'   数値を入力してください: 1
+  #'   入力された数値: 1
+readline_num = function() {
+  x = readline("数値を入力してください: ")
+  as_numeric_x = as.numeric(x)
+  cat("入力された数値:", as_numeric_x)
+}
