643fbbc451681ba365623b983481b6c5c85a179a
hihumi <sth@sth-no-MacBook-ProProPro.local>
2016-11-29 13:43:59 +0900

    fix

--- readline/readline_num.R
+++ readline/readline_num.R
@@ -8,7 +8,8 @@
   #'   数値を入力してください: 1
   #'   入力された数値: 1
 readline_num = function() {
-  x = readline("数値を入力してください: ")
+  x = readline("数値を1つ入力してください: ")
   as_numeric_x = as.numeric(x)
   cat("入力された数値:", as_numeric_x)
+  cat("\n")
 }
